2195a8674b7e66ccb001ca3f1b681338f22d8328
Aurélien Schelcher <aurelien.schelcher@gmail.com>
2019-10-18 22:36:34 +0200

    Add auth

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-
+    /**
+     * @Route("/", name="app_home")
+     */
     public function index()
     {
         return $this->render('default/index.html.twig');
